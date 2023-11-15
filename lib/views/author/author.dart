c6697a9e5d5aec5eade9ed95cf2ba1d6766dd3f8
ArtyomMB <kalashnikov252500@gmail.com>
2023-11-15 23:40:05 +1000

    add author folder

---
+++ lib/views/author/author.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+import 'package:likbez/theme/colors.dart';
+import 'package:likbez/theme/title.dart';
+import 'package:likbez/views/author/author_card.dart';
+import 'package:likbez/views/author/biography.dart';
+import 'package:likbez/widgets/books_scroll.dart';
+
+class Author extends StatelessWidget {
+    const Author({super.key});
+
+    @override
+    Widget build(BuildContext context) {
+        return Scaffold(
+            appBar: AppBar(
+                backgroundColor: black,
+                title: title
+            ),
+            body: Container(
+                decoration: const BoxDecoration(
+                    color: bgColor,
+                ),
+                child: ListView(
+                    children: const <Widget>[
+                        AuthorCard(),
+                        Biography(),
+                        BooksScroll('Произведения'),
+                ],
+              ),
+            )
+        );
+    }
+}
+
