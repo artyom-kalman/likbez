682825a31c0d6a18d920e32f51e6638dc609920d
ArtyomMB <kalashnikov252500@gmail.com>
2023-11-15 23:31:02 +1000

    update colors

--- lib/theme/colors.dart
+++ lib/theme/colors.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 
-const bgColor = Color.fromARGB(255, 12, 12, 12);
+const bgColor = Color.fromARGB(255, 29, 27, 27);
 const black   = Color.fromARGB(255, 0, 0, 0);
 const white   = Color.fromARGB(255, 255, 255, 255);
 const orange   = Color.fromARGB(255, 238, 82, 20);
