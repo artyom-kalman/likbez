a66aa17e746081e323c01c2f9c8b925e50be806f
ArtyomMB <kalashnikov252500@gmail.com>
2023-12-19 00:16:14 +1000

    Add theme

--- lib/theme/theme.dart
+++ lib/theme/theme.dart
@@ -1,15 +1,16 @@
 import 'package:flutter/material.dart';
 
-Color bgColor = Color.fromARGB(255, 12, 12, 12);
-Color black   = Color.fromARGB(255, 0, 0, 0);
-Color red     = Color.fromARGB(255, 234, 32, 40);
-Color orange  = Color.fromARGB(255, 238, 82, 20);
-Color white   = Color.fromARGB(255, 255, 255, 255);
+Color bgColor = const Color.fromARGB(255, 30, 30, 30);
+Color black   = const Color.fromARGB(255, 0, 0, 0);
+Color red     = const Color.fromARGB(255, 234, 32, 40);
+Color orange  = const Color.fromARGB(255, 238, 82, 20);
+Color white   = const Color.fromARGB(255, 255, 255, 255);
 
 var myTheme = ThemeData(
     brightness: Brightness.dark,
     scaffoldBackgroundColor: bgColor,
-    textTheme: TextTheme(
-        bodyMedium: TextStyle(color: white),
+    appBarTheme: AppBarTheme(
+        color: black,
     )
+
 );
