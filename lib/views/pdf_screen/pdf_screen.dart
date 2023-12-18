6052f2aa8ca4bb40f69cd39ba82be65cabc565d4
ArtyomMB <kalashnikov252500@gmail.com>
2023-12-18 23:37:07 +1000

    Add pages tracking

--- lib/views/pdf_screen/pdf_screen.dart
+++ lib/views/pdf_screen/pdf_screen.dart
@@ -2,11 +2,15 @@ import 'dart:async';
 import 'dart:typed_data';
 import 'package:flutter/material.dart';
 import 'package:flutter_pdfview/flutter_pdfview.dart';
+import 'package:hive/hive.dart';
+import 'package:likbez/utils/constants/boxes.dart';
+import 'package:likbez/utils/types/book_description.dart';
 
 class PDFScreen extends StatefulWidget {
     final Uint8List data;
+    final int bookId;
 
-    const PDFScreen({ super.key, required this.data });
+    const PDFScreen({ super.key, required this.data, required this.bookId });
 
     @override
     PDFScreenState createState() => PDFScreenState();
@@ -14,10 +18,32 @@ class PDFScreen extends StatefulWidget {
 
 class PDFScreenState extends State<PDFScreen> with WidgetsBindingObserver {
     final _controller = Completer<PDFViewController>();
+
     int? pages = 0;
-    int? currentPage = 0;
+    int? currentPage;
     bool isReady = false;
     String errorMessage = '';
+
+    late Box<BookDescription> bookDescriptionBox;
+
+    @override
+    void initState() {
+        super.initState();
+
+        bookDescriptionBox = Hive.box(Boxes.booksDescriptionBoxName.value);
+
+        currentPage = bookDescriptionBox.get(widget.bookId)?.currentPage;
+    }
+
+    @override
+    void dispose() {
+        super.dispose();
+
+        var book = bookDescriptionBox.get(widget.bookId)!;
+        book.currentPage = currentPage!;
+        book.pagesTotal = pages!;
+        bookDescriptionBox.put(widget.bookId, book);
+    }
 
     @override
     Widget build(BuildContext context) {
@@ -28,7 +54,7 @@ class PDFScreenState extends State<PDFScreen> with WidgetsBindingObserver {
 
 
             body: Stack(
-                children: <Widget>[
+                children: <Widget>[ 
                     PDFView(
                         pdfData: widget.data,
                         enableSwipe: true,
@@ -38,7 +64,7 @@ class PDFScreenState extends State<PDFScreen> with WidgetsBindingObserver {
                         pageSnap: true,
                         defaultPage: currentPage!,
                         fitPolicy: FitPolicy.BOTH,
-                        preventLinkNavigation: false, 
+                        preventLinkNavigation: false,  
                         onRender: (_pages) {
                             setState(() {
                                 pages = _pages;
@@ -59,7 +85,9 @@ class PDFScreenState extends State<PDFScreen> with WidgetsBindingObserver {
                         },
                         onViewCreated: (PDFViewController pdfViewController) {
                             _controller.complete(pdfViewController);
+                            pdfViewController.setPage(currentPage ?? 0);
                         },
+
                         onPageChanged: (int? page, int? total) {
                             print('page change: $page/$total');
                             setState(() {
@@ -67,19 +95,8 @@ class PDFScreenState extends State<PDFScreen> with WidgetsBindingObserver {
                             });
                         },
                     ),
-
-                    // TODO
-                    errorMessage.isEmpty
-                        ? !isReady
-                            ? Center(
-                                child: CircularProgressIndicator(),
-                                )
-                            : Container()
-                        : Center(
-                            child: Text(errorMessage),
-                            )
-                    ],
-                ),
+                ]
+            ),
         );
     }
 }
